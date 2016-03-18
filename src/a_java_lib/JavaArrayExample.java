51002ec202cbbe54d444c5c69ff06a4d4bd36bde
xiechao06 <xiechoa06@gmail.com>
2016-03-18 21:46:51 +0800

    0

---
+++ src/a_java_lib/JavaArrayExample.java
@@ -0,0 +1,15 @@
+package a_java_lib;
+
+/**
+ * Created by xc on 16-3-18.
+ */
+public class JavaArrayExample {
+
+    public void removeIndices(int[] indices) {
+
+    }
+
+    public void removeIndices2(int... indices) {
+
+    }
+}
--- src/interop.kt
+++ src/interop.kt
@@ -1,9 +1,25 @@
 import a_java_lib.A
+import a_java_lib.JavaArrayExample
+import java.util.*
+import java.util.concurrent.ThreadPoolExecutor
 
 fun test(a: A) {
     println(a.f(10))
 }
 
+fun demo(source: List<Int>) {
+
+    val list = ArrayList<Int>()
+
+    for (item in source) {
+        list.add(item)
+    }
+
+    for (i in 0..source.size-1) {
+        list[i] = source[i]
+    }
+
+}
 fun main(args: Array<String>) {
 
     test(object : A {
@@ -15,5 +31,23 @@ fun main(args: Array<String>) {
     test(A {
         x -> x * 3
     })
+
+
+    demo(listOf(1, 2, 3))
+    val calendar = Calendar.getInstance()
+    if (calendar.firstDayOfWeek == Calendar.SUNDAY) {
+        calendar.firstDayOfWeek = Calendar.MONDAY
+    }
+
+    JavaArrayExample().removeIndices(intArrayOf(1, 2, 3))
+
+    val array = intArrayOf(1, 2, 3)
+    for (i in array.indices) {
+        array[i] *= 2
+    }
+
+    JavaArrayExample().removeIndices2(*intArrayOf(1, 2, 3))
+
+    val runnable = Runnable { println("Runnable") }
 
 }
